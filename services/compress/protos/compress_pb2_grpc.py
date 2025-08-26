813d5c46730e84acc38f2115db68625a9bba6e8c
leefer <35257338+Skewary@users.noreply.github.com>
2025-08-26 17:01:47 +0800

    fix: use relative imports for generated gRPC stubs

--- services/compress/protos/compress_pb2_grpc.py
+++ services/compress/protos/compress_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import compress_pb2 as compress__pb2
+from . import compress_pb2 as compress__pb2
 
 GRPC_GENERATED_VERSION = '1.74.0'
 GRPC_VERSION = grpc.__version__
